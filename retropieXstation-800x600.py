9f08c743dbb643efc5e86efad762ab60997fc1a8
FollyMaddy <meulenf@gmx.net>
2019-09-27 19:42:38 +0200

    Click for more info
    -Improved spelling
    -Resolution is not changed when starting an .sh script from the system 'pc'
    -Program made simpler
    -If display port is not found then use another cmd line to force different resolution.

--- retropieXstation-800x600.py
+++ retropieXstation-800x600.py
@@ -7,7 +7,7 @@
 # Program made simpler
 # Version : 2.0 
 # If display port is not found then use another cmd line to force different resolution.
-# (If resolution is not possible try another monitor and/or paste and use file /usr/share/xorg.conf.d/use-vesa.conf)(reboot is nessasary)
+# (If resolution is not possible try another monitor and/or paste and use file /usr/share/xorg.conf.d/use-vesa.conf)(reboot is necessary)
 #
 # Use : 
 # retropieXstation-resolution is a simple program created to run allmost all ROMS of RetroPie from your windows desktop enviroment (X).
