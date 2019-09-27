079dc0692c5e0170601e68cbdc3a2d5e862435ea
FollyMaddy <meulenf@gmx.net>
2019-09-27 19:25:25 +0200

    Add files via upload
    -Resolution is not changed when starting an .sh script from the system 'pc'
    -Program made simpler
    -If display port is not found then use another cmd line to force different resolution.

--- retropieXstation-800x600.py
+++ retropieXstation-800x600.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python
 #
 # Program : retropieXstation-800x600.py
-# Version : 1.0
+# Version: 2.2
+# Resolution is not changed when starting an .sh script from the system 'pc'
+# Version: 2.1
+# Program made simpler
+# Version : 2.0 
+# If display port is not found then use another cmd line to force different resolution.
+# (If resolution is not possible try another monitor and/or paste and use file /usr/share/xorg.conf.d/use-vesa.conf)(reboot is nessasary)
 #
 # Use : 
 # retropieXstation-resolution is a simple program created to run allmost all ROMS of RetroPie from your windows desktop enviroment (X).
@@ -22,9 +28,9 @@
 # Or run it from the terminal with : ./retropieXstation-800x600.py
 #
 # Author : Folkert van der Meulen
-# Date   : 15/10/2018
+# Date   : 27/09/2019
 #
-# Copyright 2018 Folkert van der Meulen
+# Copyright 2019 Folkert van der Meulen
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -51,17 +57,35 @@ DISPLAY_PORT = subprocess.Popen('xrandr | grep "connected primary"| cut -d" " -f
 STANDARD_RESOLUTION = subprocess.Popen('xrandr | grep "*" | cut -d" " -f4',shell=True, stdout=subprocess.PIPE).communicate()[0][:-1]
 EMU_RESOLUTION = subprocess.Popen('xrandr | grep "800x600" | cut -d" " -f4',shell=True, stdout=subprocess.PIPE).communicate()[0][:-1]
 
+# get filename for use in commandline
+root = tk.Tk()
+root.withdraw()
+ROM = tkFileDialog.askopenfilename(initialdir = '~/RetroPie/roms' , title='Load RetroPie Game') 
+# cut system type from directory structure for use in commandline
+SYSTEM = ROM.split("/", ROM.count("/"))[5]
+EXTENSION=ROM.split(".", ROM.count("."))[-1]
+
 # only execute if resolution of 800x600 is possible
 if EMU_RESOLUTION == '800x600':
-    # get filename for use in commandline
-    root = tk.Tk()
-    root.withdraw()
-    ROM = tkFileDialog.askopenfilename(initialdir = '~/RetroPie/roms' , title='Load RetroPie Game') 
-    # cut system type from directory structure for use in commandline
-    SYSTEM = ROM.split("/", ROM.count("/"))[5]
-    # create commandline as string (change resolution for old pc's, insert system type and filename to run)
-    cmd_run_emu = 'xrandr --output %s --mode "%s" && /opt/retropie/supplementary/runcommand/runcommand.sh 0 _SYS_ %s "%s"' %(DISPLAY_PORT,EMU_RESOLUTION,SYSTEM,ROM)
-    # run command
-    os.system(cmd_run_emu)
-    # change to standard resolution
-    os.system('xrandr --output %s --mode "%s"' %(DISPLAY_PORT,STANDARD_RESOLUTION)) 
+
+    #make an exeption for system 'pc' .sh scripts, do not change resolution
+    if EXTENSION == 'sh' and SYSTEM == 'pc':
+        EMU_RESOLUTION = STANDARD_RESOLUTION
+
+    # if a diplay port is not found then use execute these lines, good chance it will work
+    if not DISPLAY_PORT:
+       # create commandline as string (change resolution for old pc's, insert system type and filename to run)
+       cmd_run_emu = 'xrandr -s "%s" && /opt/retropie/supplementary/runcommand/runcommand.sh 0 _SYS_ %s "%s"' %(EMU_RESOLUTION,SYSTEM,ROM)
+       # run command
+       os.system(cmd_run_emu)
+       # change to standard resolution
+       os.system('xrandr -s "%s"' %(STANDARD_RESOLUTION)) 
+   
+    # if a diplay port is found then use execute these lines, it is more precise but will not work if the display port is not found
+    else:
+       # create commandline as string (change resolution for old pc's, insert system type and filename to run)
+       cmd_run_emu = 'xrandr --output %s --mode "%s" && /opt/retropie/supplementary/runcommand/runcommand.sh 0 _SYS_ %s "%s"' %(DISPLAY_PORT,EMU_RESOLUTION,SYSTEM,ROM)
+       # run command
+       os.system(cmd_run_emu)
+       # change to standard resolution
+       os.system('xrandr --output %s --mode "%s"' %(DISPLAY_PORT,STANDARD_RESOLUTION)) 
